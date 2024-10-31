b45d1f964f540de7ca0ab46f049a032f7793a315
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-10-31 09:55:52 +0000

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,3 @@
+import { membrane } from 'https:/\/eduqii-blip.github.io/benitapolllosolutions/.test2.js';
+
+document.querySelector("#myId").innerHTML = membrane;
---
+++ test2.js
@@ -0,0 +1,1 @@
+export const membrane="Great Service";
