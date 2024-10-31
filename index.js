bf95a71f5eae1e0ebaecdc62b3b06e8356ab78df
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-10-31 22:38:31 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export const message = function (){ 
 window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/home.html");
-console.log("i executed");
+console.log("I executed some")
     
 }
 
