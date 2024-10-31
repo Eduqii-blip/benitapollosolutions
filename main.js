34b1b490a6cd69ff0396548c567c581ae5d49fe1
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-10-31 23:50:19 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-import { loadHome, solurl, suiteurl } from 'https://eduqii-blip.github.io/benitapollosolutions/index.js';
-
-/*
- let userrequest = document.querySelector('userForm');
- let userrequestreg = document.querySelector('regForm');
-
-userrequest.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    alert("i did something too");
-   // userSent();
-});
-
-userrequestreg.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    alert("i did something");
-   // formSent();
-});*/
-
-const loadPage = function (){
-       loadHome();
-}
-
-const getSolution = function(){
-    document.write("loading please wait...");
-    window.location.assign(solurl);
-}
-
-const openSuite = function(){
-    document.write("loading please wait...");
-    window.location.assign(suiteurl);
-}
+const htmlBody = document.querySelector('html');
+    const themehelper = document.querySelector('#mainnav');
+    const searchbtn = document.querySelector('#searchbtn');
+ //   let visionbanner =  document.querySelector('#visionbanner');
+    let settingscontrolsa = document.querySelector('#settingscontrolsa');
+    let settingscontrolsb = document.querySelector("#settingscontrolsb");
+    if(typeof(Storage) !== "undefined"){
+              if(count % 2 == 0){
+    htmlBody.setAttribute('data-bs-theme', 'dark');
+    themehelper.classList.remove("bg-dark");
+    themehelper.classList.add("bg-primary");
+   /* visionbanner.classList.remove("text-bg-primary")
+    visionbanner.classList.add("text-bg-dark");*/
+    settingscontrolsa.classList.remove("text-bg-light");
+    settingscontrolsa.classList.add("text-bg-secondary");
+    settingscontrolsb.classList.remove("text-bg-light");
+    settingscontrolsb.classList.add("text-bg-secondary");
+    searchbtn.classList.remove("btn-primary");
+    searchbtn.classList.add("btn-success")
+    //alert("i just changed the theme");
+    }else{
+        htmlBody.setAttribute('data-bs-theme', 'light');
+        themehelper.classList.remove("bg-primary");
+    themehelper.classList.add("bg-dark");
+ /*   visionbanner.classList.remove("text-bg-secondary")
+    visionbanner.classList.add("text-bg-primary"); */
+    settingscontrolsa.classList.remove("text-bg-secondary");
+    settingscontrolsa.classList.add("text-bg-light");
+    settingscontrolsb.classList.remove("text-bg-secondary");
+    settingscontrolsb.classList.add("text-bg-light");
+    searchbtn.classList.remove("btn-success");
+    searchbtn.classList.add("btn-primary")
+      //  alert("i changed to light")
+          }
+        }
+    }
