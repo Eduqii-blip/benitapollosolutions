a8af487f5421ae2e54581965466c8218744fc282
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-10-31 22:31:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export const message = function (){ 
 window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/index.html");
-alert("i executed");
+console.log("i executed");
     
 }
 
