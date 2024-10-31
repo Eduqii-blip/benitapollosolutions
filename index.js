3e4bd91e133b23f0e0d5cd04d7af75e3a98b11a2
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-10-31 21:32:18 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 export const message = function (){
- alert("hello world!");
+    let loadstatus = document.createElement("div");
+    let headstatus = document.createElement("h1");
+    let _load = document.createElement("span");
+    loadstatus.classList.add("container");
+    loadstatus.classList.add("mt-5");
+    headstatus.classList.add("display-3");
+    _load.classList.add("spinner-border");
+    loadstatus.appendChild(headstatus);
+    loadStatus.innerText = "Loading.. please wait";
+    headstatus.appendChild(_load);
+    
+    
 }
+
