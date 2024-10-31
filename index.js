f461273e1471d620bd617a7ef890903193fe0058
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-10-31 21:50:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 export const message = function (){
-    let loadstatus = document.createElement("div");
-    let headstatus = document.createElement("h1");
-    let _load = document.createElement("span");
-    loadstatus.classList.add("container");
-    loadstatus.classList.add("mt-5");
-    headstatus.classList.add("display-3");
-    _load.classList.add("spinner-border");
-    loadstatus.appendChild(headstatus);
-    headstatus.appendChild(_load);
+    alert("hello world");
     
     
 }
