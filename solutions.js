118bc0f60e54938b0bfd35678449f47f1823459b
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-11-05 07:48:38 +0000

    Add files via upload

--- solutions.js
+++ solutions.js
@@ -1,12 +1,14 @@
   "use strict";
   let myVideo = document.getElementById("inclass");
-            myVideo.addEventListener("timeupdate", function(){
+  if(myVideo !== null){
+      myVideo.addEventListener("timeupdate", function(){
     if(this.currentTime >= 5 * 14) {
         this.pause();
         
         window.location.assign("https://tinyurl.com/25s9w8wq")
     }
 });
+  }
 
 $(window).on('load', function() {
         $('#salutationModal').modal('show');
