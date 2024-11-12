55fd899e3b3415eb61d30154a94f9fdbbec2a4cc
Eduqii-blip <80406656+Eduqii-blip@users.noreply.github.com>
2024-11-12 19:08:12 +0000

    Add files via upload

--- site.js
+++ site.js
@@ -10,7 +10,12 @@
                     .then(() => {
                         console.log('SUCCESS!');
                         document.write("information sent");
-                        window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/studysuccess.html");
+                        if(location.href="https://eduqii-blip.github.io/benitapollosolutiolns/educationstudy.html"){
+                            window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/studysuccess.html");
+                        } 
+                        else if(location.href="https://eduqii-blip.github.io/benitapollosolutions/educationsolution.html"){
+                            window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/test.html")
+                        }
                     }, (error) => {
                         console.log('FAILED...', error);
                     });
@@ -18,14 +23,4 @@
             },true);     
         }
         
-        document.getElementById('study-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                emailjs.sendForm('service_je7v6yn', 'Study_hjzp2fe', this)
-                    .then(() => {
-                        console.log('SUCCESS!');
-                        document.write("information sent");
-                        window.location.assign("https://eduqii-blip.github.io/benitapollosolutions/studysuccess.html");
-                    }, (error) => {
-                        console.log('FAILED...', error);
-                    });
-            },true);        
+        
